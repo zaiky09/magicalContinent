e45883c5f9304558b029dc18e6cc0cfc1955ce31
zaiky09 <zamilmozamil@outlook.com>
2025-02-04 17:12:59 +0300

    changed ticket booking picture

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -6,7 +6,7 @@ import Hammock from "../../public/images/Hammock.jpg";
 import Amsterdam from "../../public/images/Amsterdam.jpg";
 import Baloons from "../../public/images/airBaloons.jpg";
 import Giraffe3 from "../../public/images/Giraffe3.jpg";
-import Ghana from "../../public/images/Ghana.jpg";
+// import Ghana from "../../public/images/Ghana.jpg";
 import Egypt1 from "../../public/images/Egypt1.jpg";
 import SouthAfrica from "../../public/images/SouthAfrica.jpg";
 import Lion from "../../public/images/LionElephants.jpg";
@@ -14,6 +14,8 @@ import Leopard from "../../public/images/Buffalo.jpg";
 import Meercats from "../../public/images/Meercats.jpg";
 import Desert from "../../public/images/Desert.jpg";
 import Crane from "../../public/images/Crane.jpg";
+import Nigeria from "../../public/images/Nigeria.jpg";
+// import Senegal from "../../public/images/Senegal1.jpg";
 
 const Gallery = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -22,8 +24,8 @@ const Gallery = () => {
     Meercats,
     Desert,
     Crane,
+    Nigeria,
     Baloons,
-    Ghana,
     Amsterdam,
     Lion,
     Hammock,

94accb6acad0c93380eb029d849e4d3f5bfb3a51
zaiky09 <zamilmozamil@outlook.com>
2025-02-04 21:10:45 +0300

    Added more pictures to gallery

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -2,11 +2,11 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
-import Hammock from "../../public/images/Hammock.jpg";
+import Madagascar from "../../public/images/Madagascar.jpg";
 import Amsterdam from "../../public/images/Amsterdam.jpg";
 import Baloons from "../../public/images/airBaloons.jpg";
 import Giraffe3 from "../../public/images/Giraffe3.jpg";
-// import Ghana from "../../public/images/Ghana.jpg";
+import Seychelles from "../../public/images/Seychelles.jpg";
 import Egypt1 from "../../public/images/Egypt1.jpg";
 import SouthAfrica from "../../public/images/SouthAfrica.jpg";
 import LionCubs from "../../public/images/LionCubs.jpg";
@@ -14,25 +14,30 @@ import Buffalo from "../../public/images/Buffalo.jpg";
 import Meercats from "../../public/images/Meercats.jpg";
 import Desert from "../../public/images/Desert.jpg";
 import Crane from "../../public/images/Crane.jpg";
-import Nigeria from "../../public/images/Nigeria.jpg";
-// import Senegal from "../../public/images/Senegal1.jpg";
+import Dikdik from "../../public/images/Dikdik.jpg";
+import Gorillas from "../../public/images/Gorillas.jpg";
+import VictoriaFalls from "../../public/images/VictoriaFalls.jpg";
 
 const Gallery = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const images = [
     Meercats,
-    Desert,
+    VictoriaFalls,
     Crane,
-    Nigeria,
     Baloons,
     Amsterdam,
-    LionCubs,
-    Hammock,
+    Seychelles,
     Buffalo,
+    LionCubs,
+    Dikdik,
     Egypt1,
     Giraffe3,
-    SouthAfrica
+    SouthAfrica,
+    Madagascar,  
+    Desert,
+    Gorillas
+    
   ];
 
   const nextImages = () => {
